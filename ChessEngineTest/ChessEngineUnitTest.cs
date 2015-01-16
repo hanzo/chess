19458052c971ce98e4ff1c791613d17969863446
hanzo <hanzo87@gmail.com>
2015-01-15 22:13:59 -0800

    Add unit test project

---
+++ ChessEngineTest/ChessEngineUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using Chess.ChessEngine;
+using NUnit.Framework;
+
+namespace ChessEngineTest
+{
+	[TestFixture]
+	public class ChessEngineUnitTest
+	{
+		[Test]
+		public void TurnZeroActivePlayer()
+		{
+			Match match = new Match(PieceColor.Black, "White", "Black");
+			Assert.That(match.ActivePlayer.Color, Is.EqualTo(PieceColor.White));
+		}
+	}
+}
